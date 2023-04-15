90e2980c705ed9aeb429e0ae60c0f519ac024f74
Anya <anyafleming16@gmail.com>
2023-04-15 17:31:15 -0500

    I finished lab-02

---
+++ js/app.js
@@ -0,0 +1,43 @@
+alert("Welcome in")
+
+// TODO: Ask the user's name.
+
+// TODO: Ask 5 y/n questions.
+let answerOne = prompt("Do I have a dog?")
+let answerTwo = prompt("Do I like cats?")
+let answerThree = prompt("Do I have children?")
+let answerFour = prompt("Do I like the color black?")
+let answerFive = prompt("Do I like sports?")
+
+if(answerOne === 'n' || answerOne ==='no' ){
+    console.log("You are correct!");
+} else {
+    // if 'y' then you are incorrect
+    console.log("You are incorrect!");
+}
+
+if(answerTwo === 'n' || answerTwo === 'no' ){
+    console.log("You are correct!");
+}else {
+    // if 'y' then you are incorrect
+    console.log("You are incorrect!")
+}
+
+if(answerThree === 'y' || answerThree ==='yes' ){
+    console.log("You are correct!");
+} else {
+    // if 'y' then you are incorrect
+    console.log("You are incorrect!");
+} 
+if(answerFour === 'y' || answerFour ==='yes' ){
+    console.log("You are correct!");
+} else {
+    // if 'y' then you are incorrect
+    console.log("You are incorrect!");
+} 
+if(answerFive === 'y' || answerFive==='yes' ){
+    console.log("You are correct!");
+} else {
+    // if 'y' then you are incorrect
+    console.log("You are incorrect!");
+} 
